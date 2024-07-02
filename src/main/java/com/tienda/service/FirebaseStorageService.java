d64820a039a51a23779cf9d894332e6417ad3016
z8w33ss2 <123525733+z8w33ss2@users.noreply.github.com>
2024-07-01 20:58:07 -0600

    Semana 06

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author user
+ */
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-f1f86.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-f1f86-firebase-adminsdk-30iaw-8c440bed41.json";
+
+}
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -18,17 +18,35 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
+
     @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-        var lista=categoriaDao.findAll();
+        var lista = categoriaDao.findAll();
         if (activos) {
-           lista.removeIf(e -> !e.isActivo());
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);// Guardar o modificar el id
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria); //Eliminar el id categoria
     }
 }
